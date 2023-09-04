0910431f9d995151b0fc51c8c885023d0e2ea022
Assaf Stone <1774556+devopsjester@users.noreply.github.com>
2023-09-04 23:57:56 +0000

    can get the weather

--- index.ts
+++ index.ts
@@ -8,6 +8,12 @@ interface ZipData {
     }[];
 }
 
+interface WeatherData {
+    main: {
+        temp: number;
+    };
+}
+
 const argv = yargs(process.argv.slice(2))
     .command('$0', 'get city and state from a zip code', (yargs) => {
         yargs.option('zip', {
@@ -21,12 +27,24 @@ const argv = yargs(process.argv.slice(2))
     .argv;
 
 const zip = argv.zip as string;
+const apiKey = process.env.OPENWEATHERMAP_API_KEY;
+
+if (!apiKey) {
+    console.error('OPENWEATHERMAP_API_KEY environment variable not set');
+    process.exit(1);
+}
 
 axios
     .get<ZipData>(`https://api.zippopotam.us/us/${zip}`)
     .then((response) => {
         const { state, 'place name': city } = response.data.places[0];
         console.log(`City: ${city}, State: ${state}`);
+
+        return axios.get<WeatherData>(`https://api.openweathermap.org/data/2.5/weather?q=${city},${state}&appid=${apiKey}&units=imperial`);
+    })
+    .then((response) => {
+        const temperature = response.data.main.temp;
+        console.log(`Temperature: ${temperature}°F`);
     })
     .catch((error) => {
         console.error(`Error fetching data: ${error.message}`);
